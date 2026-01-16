104022cce4bb06a8e954d425da0657d177fa6e92
laurenzotadjio1999-wq <laurenzotadjio1999@gmail.com>
2026-01-16 15:55:34 -0500

    # Création d’un message de bienvenue

--- interface_graphique.py
+++ interface_graphique.py
@@ -1,5 +1,5 @@
 # Importation des packages nécessaires
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QTableWidget, QTableWidgetItem, QLabel, QGridLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QTableWidget, QTableWidgetItem, QLabel,QGridLayout
 import gestion_donnees
 import operations_CRUD
 
@@ -8,6 +8,16 @@ app = QApplication([])
 fenetre = QWidget()
 fenetre.setWindowTitle("TP2_POO_Laurenzo-Ricardeau")
 fenetre.setGeometry(200, 200, 650, 500)
+
+# Création d’un message de bienvenue
+message = "*********************************************************\n"
+message += "*       GESTION DU PARC INFORMATIQUE – TP2 POO          ***\n"
+message += "*********************************************************"
+label_bienvenue = QLabel(message)
+grid.addWidget(label_bienvenue, 0, 2, 1, 6)
+label_bienvenue.setStyleSheet("font-family: Calibri; font-weight: bold;")
+
+
 
 
 # Affichage de la fenêtre et exécution de l’application
